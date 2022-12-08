6153e141a09b8cce3b2f16683bb7020de9eff24d
Adam56g <gillespie-a35@ulster.ac.uk>
2022-12-08 17:31:33 +0000

    alot

--- src/JS/app.js
+++ src/JS/app.js
@@ -1,6 +1,7 @@
 //The URIs of the REST endpoint
 IUPS = "https://prod-23.uksouth.logic.azure.com:443/workflows/31b91635684d43cb906d4bf288fc3b2c/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=5lD7rCQWpCA9y60YzFbJ2omAhvfXs5GcLTJUz8xzMuc";
 RAI = "https://prod-10.uksouth.logic.azure.com:443/workflows/558123aedeff4f89b7a618f001805a67/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=ngcIeXHqfzpXmXW5a7RuVWUVjBoVGV0JQISDJo8H3rY";
+LOGIN_VAL = "https://prod-15.uksouth.logic.azure.com:443/workflows/1c60d493141b433a971365804eaf476d/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=_jrxcIxVa7BV848SS6Yt2jClH02RsTmE-utjZWPvW20";
 
 BLOB_ACCOUNT = "https://blobstorageb00836889.blob.core.windows.net";
 
@@ -77,5 +78,55 @@ html: items.join( "" )
 }).appendTo( "#VideosList" );
 });
 
+}
+
+function loginvalidation(){
+
+  var nameduser = document.getElementById("student").value;
+  var userpass = document.getElementById("password123").value;
+
+  LoginCheck =new FormData();
+
+  LoginCheck.append('student',nameduser)
+  LoginCheck.append('password123',userpass)
+
+  console.log(nameduser)
+  console.log(userpass)
+
+  $.ajax({
+ url: LOGIN_VAL,
+ data: LoginCheck,
+ cache: false,
+ contentType: false,
+ processData: false,
+ type: 'POST',
+ success: function(userinfo){
+  console.log(userinfo)
+  console.log(userinfo[0])
+
+  username = userinfo[0].username;
+  forename = userinfo[0].forename;
+  surname = userinfo[0].surname;
+  userID = userinfo[0].userID;
+  Privileges = userinfo.Privileges;
+
+  console.log(forename)
+
+  if(userinfo[0].username){
+    if(Privileges){
+      alert("Welcome Admin!");
+      window.location="./index.home.html";
+    }else {
+      alert("Welcome User!");
+      window.location="index.home.users.html";
+    }
+  }else{
+    alert("Unsuccessful Login")
+  }
+  return false;
+ }
+
+
+})
 }
 
